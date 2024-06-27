c91961f03d8c81f825173b9cfb7c63c76dd11b61
Tuan Nguyen <anhtuann0210@gmail.com>
2024-06-27 20:34:42 +0700

    add log controller

---
+++ src/WebAPI/Controllers/LogController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog.Context;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "LogInformation")]
+        public IActionResult LogInformation([FromBody] Input input)
+        {
+            _logger.LogInformation("Body input {@body}", input);
+
+            return Ok();
+        }
+
+        [HttpPost("/information-with-log-context")]
+        public IActionResult LogInformationWithLogContext([FromBody] Input input)
+        {
+            //using (LogContext.PushProperty("CorrelationId", Guid.NewGuid()))
+            //using (LogContext.PushProperty("Method", "POST"))
+            //{
+            //    _logger.LogInformation("Body {@body}", input);
+            //}
+
+            using (LogContext.PushProperty("CorrelationId", Guid.NewGuid()))
+            using (LogContext.PushProperty("Method", "POST"))
+                _logger.LogInformation("Body {@body}", input);
+
+            return Ok();
+        }
+    }
+
+    public class Input
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
